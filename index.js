ae20910a7a67236e01d47cae658bd5e3385a3443
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-08-06 18:26:09 +0100

    API upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const projects = require("./routes/projects");
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/projects", projects);
+
+app
+  .get("/api", (req, res) => {
+    res.send("Hello World <h1> from Express </h1>");
+  })
+  .listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  })
+  .on("error", err => {
+    console.log(err);
+  })
+  .on("listening", () => {
+    console.log("Server started on port 5000");
+  })
+  .on("close", () => {
+    console.log("Server closed");
+  })
+  .on("connection", () => {
+    console.log("Connection established");
+  });
+
+module.exports = app;
