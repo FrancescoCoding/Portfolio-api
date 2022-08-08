ab8fe2499117050efe09c121e55fbc7c0b734f78
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-08-08 11:24:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const projects = require("./routes/projectsRoutes");
 
 // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
+
+// Set access control for the app
+app.use((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "fran.netlify.app");
+});
+
 app.use("/projects", projects);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
