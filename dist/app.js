c95d24251a027dfe5013da8525d01f0222f63194
Francesco Gruosso <64712227+FrancescoCoding@users.noreply.github.com>
2022-08-28 23:38:15 +0100

    Update [Names]

--- dist/app.js
+++ dist/app.js
@@ -7,12 +7,12 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 require("dotenv/config");
 const db_1 = require("./database/db");
-const projects_1 = __importDefault(require("./routes/projects"));
+const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
 const errorMiddleware_1 = require("./middleware/errorMiddleware");
 (0, db_1.connectDB)();
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.use("/api", projects_1.default);
+app.use("/api", projectRoutes_1.default);
 app.use(errorMiddleware_1.errorHandler);
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
